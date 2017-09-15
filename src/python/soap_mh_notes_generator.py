aac33ef14fc56e9f8b85eabdfa70317ad50bd1db
Edmon Begoli <ebegoli@gmail.com>
2017-09-14 23:50:42 -0400

    Adding preliminary version

---
+++ src/python/soap_mh_notes_generator.py
@@ -0,0 +1,69 @@
+""" Translated from University of Chicago Psychiatry Clerkship, Pritzker School of Medicine, retrived 9/14/2015 """
+header = "${date} and ${time}"
+
+subjective = "Subjective:${symptoms} ${course}"
+
+collateral_information = "Collateral information" "Stresses"
+
+staff_observations = "Staff observations" = ["${agitation$}, ${sleep log$}, ${cooperation}, ${behavior}"]
+
+medications = "Medications" = ["${name, dose, route, frequency, duration, therapeutic and side effects, compliance}"]
+
+other_treatments = "Other treatments:" = "${ECT (treatment #, and as with medications)}$, ${psychotherapy}"
+
+objective = "Objective:"
+
+appearance = "Appearance:" ="${gait ____}, ${posture ____}, ${clothes ____}, ${grooming} ,${mileage}"
+
+general_behavior = "General behavior: mannerisms ____, gestures ____, psychomotor activity ____, expression ____,"+
+",eye contact,able to follow commands/requests,compulsions"
+
+attitude = "Attitude: "," cooperative, "," hostile, "," defensive, "," open, "," secretive, "," evasive, "," suspicious, "," apathetic,"+
+"easily distracted,focused, etc."
+
+LOC = "LOC: "," vigilant, "," alert, "," drowsy, "," lethargic, "," stuporous, "," asleep, "," comatose, "," confused, "," fluctuating, etc."
+
+attention = "Attention: "," can attend, "," can concentrate, "," distractible, "," digit span, "," DLROW, "," calculations"
+
+orientation = "Orientation: "," person, "," place, "," time, "," situation"
+
+memory = "Memory: "," immediate, "," recent, "," remote"
+
+intellectual = "Intellectual: fund of knowledge ____, vocabulary ____"
+
+MMSE = "MMSE: __ points out of 30 achieved, missing the following tasks ____"
+
+speech = "Speech: volume/tone (loud,soft,monotone,weak,strong), rate (fast,slow,normal,pressured),"+
+"amount (talkative,spontaneous,expansive,paucity,poverty), fluency/rhythm (slurred,clear,"+
+",with appropriately placed inflections, "," hesitant, "," with good articulation, "," aphasic)"
+mood = "Mood (inquired): "," depressed, "," euthymic, "," elevated, "," euphoric, "," irritable, "," anxious, etc."
+affect = "Affect (observed): "," appropriate to situation, "," consistent with mood, "," congruent with thought content,"+
+"fluctuations ("," labile, "," even), range ("," broad, "," restricted), intensity ("," blunted, "," flat, "," normal),"+
+"quality ("," sad, "," angry, "," hostile, "," indifferent, "," euthymic, "," dysphoric, "," detached, "," elated,"+
+" euphoric, "," anxious, "," animated, "," irritable)"
+thought_process = "Thought process/form: "," linear, "," goal-directed, "," circumstantial, "," tangential, "," loose associations,"+
+",incoherent,evasive,racing,thought blocking,perseveration,neologisms"
+thought_content = "Thought content: "," delusions (type: "," paranoid/persecutory, "," grandiose, "," somatic, "," religious, "," bizarre,"+
+"nihilistic,thought insertion, thought withdrawal, thought broadcasting), ideas of reference,"+
+"magical thinking, illusions/hallucinations (type: auditory, visual, somatic, olfactory,"+
+",gustatory, command, conversation, commentary), "," suicide/homicide (elaborate further:"+
+",active,passive,hopelessness,plan,detailed plan,partially executed plan,method available,"+
+",impulsivity,command hallucinations,intention,contracting), phobias (type:,social,"+
+",agoraphobia,specific/simple),obsessions"
+
+insight_judgment = "Insight/Judgement:","aware of problem/role","abstract (similarities, proverbs),understand facts,draw conclusions","problem solving"
+
+lab_tests = "Laboratory & other tests"
+
+assessment = "Assessment:Working primary diagnosis, current differential, other diagnoses"
+axis_I = "DSM-IV Axis I:"
+axis_II = "DSM-IV Axis II:"
+axis_III = "DSM-IV Axis III:"
+
+plan = "Plan:"
+diagnostic="Diagnostic: obtain collateral information, further observation or questioning, tests, etc."
+specific_treatment = "Specific treatment: based upon diagnosis, e.g., medication titration, how many more ECT sessions, etc."
+general_treatment = "General treatment: reduce stresses, environmental modification, symptomatic (e.g., insomnia), education"
+
+disposition = "Disposition: next source of care, follow-up, back-up plans"
+reporting_physician = "$md_name$, title (e.g., MS3), pager number, and sign"
