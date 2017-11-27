09eb88d2e7b3876bf31dacbfe013764af3e9d419
Sudarshan Srinivasan <sudarshan85@gmail.com>
2017-11-27 18:30:18 -0500

    Added required argparse args

--- src/python/note_gen.py
+++ src/python/note_gen.py
@@ -2,21 +2,31 @@
 
 import random
 import json
+import argparse
 
 from string import Template
 
-def main():
-    with open('test.base', 'r') as base_file:
-        base = base_file.read()
+def main(base_file, subs_file, n_notes):
+    print("Reading from {} file".format(base_file))
+    with open(base_file, 'r') as fh:
+        base = fh.read()
 
-    with open('values.json', 'r') as value_file:
-        values = json.load(value_file)
+    print("Reading from {} file".format(subs_file))
+    with open(subs_file, 'r') as fh:
+        subs = json.load(fh)
 
     t = Template(base)
-    d = {k: random.choice(v) for k, v in values.items()}
+    d = {k: random.choice(v) for k, v in subs.items()}
 
-    note = t.safe_substitute(d)
+    print("Generating {} synthetic notes".format(n_notes))
+    note = t.safe_substitute(d).strip()
     print(note)
 
 if __name__=='__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-bf', '--baseFile', help='Base file with format', required=True)
+    parser.add_argument('-sf', '--subsFile', help='JSON file with substitutes', required=True)
+    parser.add_argument('-n', '--number', help='Number of notes required (default 10)', default=10)
+    args = parser.parse_args()
+    main(args.baseFile, args.subsFile, args.number)
+    # main()
