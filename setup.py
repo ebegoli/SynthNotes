96e3855275b1232294ddd4277782ad55987fbd76
Kris Brown <kris.brown3901@gmail.com>
2017-12-12 16:37:53 -0500

    Removed some comments

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ from setuptools import setup, find_packages
 I'm certain this needs to be filled out more and have more fields added to setup.
 I'm not familiar with those so I added the bare bones info for now.
 Seems we need to include:
-    packages
-    install_requires
     entry_points:console_scripts
     
 Also need to decide on how we will do version schemes.  
