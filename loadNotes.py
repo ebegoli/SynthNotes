b53f423d0a5f9a84b75f182e81582270dcc7e6be
Kris Brown <kb0@ornl.gov>
2018-06-26 02:19:13 -0400

    merge

--- loadNotes.py
+++ loadNotes.py
@@ -6,27 +6,38 @@ def main():
 
     generator = NoteGenerator()
 
-    params = {'database': 'synthea', 'user': 'postgres', 'host': '172.22.10.147'}
+    params = {'database': 'synthea', 'user': 'postgres', 'host': '172.22.8.180'}
     conn = psy.connect(**params)
 
-    cur = conn.cursor()
-
-    cur.execute("""select id, person_id, start, stop from encounter 
-                where display like 'Psychiatric procedure or service' limit 10;
+    cur = conn.cursor("reader")
+    curWrite = conn.cursor()
+    cur.execute("""select e.id, e.person_id, e.start, e.stop 
+                    FROM encounter e
+                    LEFT JOIN notes n
+                    on e.id = n.encounter_id
+                    WHERE e.system = 'SNOMED-CT' 
+                        and e.code = '24642003' 
+                        and n.encounter_id is NULL;
                 """)
-    results = cur.fetchall()
 
-    for row in results:
+    count = 0
+    for row in cur:
         encounter_id = row[0]
         person_id = row[1]
         start = row[2]
         stop = row[3]
 
         note = generator.generate()
-        cur.execute("""
+        curWrite.execute("""
             INSERT into notes(encounter_id, person_id, note, start, stop)
-            Values (%s, %s, %s, %s)
-            """, encounter_id, person_id, note, start, stop)
+            Values (%s, %s, %s, %s, %s)
+            """, [encounter_id, person_id, note, start, stop])
+        count += 1
+
+    print(f"wrote {count} notes")
+
+    conn.commit()
+    conn.close()
 
 
 if __name__ == '__main__':
